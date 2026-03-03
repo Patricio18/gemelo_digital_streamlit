b6bbf72785e3be340f665635eeb73f29234c4b5d
Patricio Acevedo <jpaa_acevedo918@gmail.com>
2026-03-03 13:09:04 -0600

    act

--- microalgasLab.py
+++ microalgasLab.py
@@ -639,9 +639,9 @@ with right_column:
     # 1. Preparar los datos (igual que arriba)
     df = pd.DataFrame({
         'Horas': range(10),
-        'Biomasa': exp_chlorella[0:10],
-        'Nitrato':exp_scenedesmus[0:10],
-        'CO2': exp_planktothrix[0:10]
+        'Biomasa': exp_chlorella[0:11],
+        'Nitrato':exp_scenedesmus[0:11],
+        'CO2': exp_planktothrix[0:11]
     })
 
     # 2. Crear la gráfica con Plotly
