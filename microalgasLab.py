59259d0f44e7315ce89a990a0ed7a237a7ac3e6b
Patricio Acevedo <jpaa_acevedo918@gmail.com>
2026-03-05 13:18:25 -0600

    ok

--- microalgasLab.py
+++ microalgasLab.py
@@ -182,7 +182,7 @@ with left_column:
     with mumax_scenedesmus_column:
         mu_maxScenedesmus = st.number_input(":blue[Scenedesmus]", min_value=0.0, max_value=5.0, value=1.5, step=0.01)
     with mumax_planktothrix_column:
-        mu_maxPlanktothrix = st.number_input(":purple[Planktothrix]", min_value=0.0, max_value=5.0, value=2.0, step=0.01)
+        mu_maxPlanktothrix = st.number_input(":orange[Planktothrix]", min_value=0.0, max_value=5.0, value=2.0, step=0.01)
 
     st.markdown("##### Coeficiente de sustrato")
     kn_chlorella_column, kn_scenedesmus_column, kn_planktothrix_column = st.columns(3)
@@ -191,7 +191,7 @@ with left_column:
     with kn_scenedesmus_column:
         kn_scenedesmus = st.number_input(":blue[Scenedesmus]", min_value=0.0, max_value=10.0, value=2.5, step=0.1)
     with kn_planktothrix_column:
-        kn_planktothrix = st.number_input(":purple[Planktothrix]", min_value=0.0, max_value=10.0, value=2.5, step=0.1)
+        kn_planktothrix = st.number_input(":orange[Planktothrix]", min_value=0.0, max_value=10.0, value=2.5, step=0.1)
 
 with center_column:
     st.subheader('Visualización')
