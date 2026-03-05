550f26e17bf0e64fb541a81f547a5389d57bc23f
Patricio Acevedo <jpaa_acevedo918@gmail.com>
2026-03-05 15:59:37 -0600

    okas jeje

--- microalgasLab.py
+++ microalgasLab.py
@@ -550,9 +550,9 @@ with right_column:
     
     cantidad_inicial = 100
     
-    #def monod(mu_max, y, N, Kn): 
-    #    mu = - (mu_max/y) * (N/(Kn + N)) * cantidad_inicial
-    #    return mu
+    def monod2(mu_max, y, N, Kn): 
+        mu = - (mu_max/y) * (N/(Kn + N)) * cantidad_inicial
+        return mu
     st.divider()
 
 
